223fd8dbae940177b83d0a1e02b5408881381183
Ritik Channa <chnritik@gmail.com>
2018-08-11 14:50:38 +0530

    Support n-input in and-gates

--- and_gate.v
+++ and_gate.v
@@ -1,23 +1,24 @@
 `timescale 1ns / 1ps
-
+//2-input and-gate using gate level modelling.
 module and_gate(o,a,b);
 input a,b;
 output o;
 and a1(o,a,b);
 endmodule
 
-
-module and_dataflow(o,a,b);
-input a,b;
+//n-input and-gate using dataflow modelling.
+module and_dataflow(o,i);
+parameter inputs = 2;
+input [inputs:1] i;
 output o;
-assign o = a & b;
+assign o = &i;
 endmodule
 
-
-module and_behav(o,a,b);
-input a,b;
-output o;
-reg o;
-always @ (a,b)
-o = a & b;
+//n-input and-gate using behavioural modelling. 
+module and_behav(o,i);
+parameter inputs = 2;
+input [inputs:1] i;
+output reg o;
+always @ (i)
+o = &i;
 endmodule
