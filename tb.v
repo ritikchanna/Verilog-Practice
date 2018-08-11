c57a272d03694633ac9a49a76c8f7c5e1c45975c
Ritik Channa <chnritik@gmail.com>
2018-08-11 14:51:31 +0530

    Update tb for andgates

--- tb.v
+++ tb.v
@@ -2,16 +2,14 @@
 module tb();
 wire o1,o2,o3;
 reg i1,i2,i3;
-and_behav a1(o1,i2,i3);
-and_dataflow a2(o2,i2,i3);
-and_gate a3(o3,i2,i3);
-
+and_behav 	#(3) a1(o1,{i1,i2,i3});
+and_dataflow #(3) a2(o2,{i1,i2,i3});
+and_gate	   #(3) a3(o3,{i1,i2,i3});
 initial
 	$monitor("%t  i1=%d ,i2=%d ,i3=%d ,o1=%d ,o2=%d ,o3=%d",$time,i1,i2,i3,o1,o2,o3);
 
 initial
 begin
-$display("TestBench Running");
 	i1 = 0; i2 =0; i3=0;
 #5 i1 = 0; i2 =0; i3=1;
 #5 i1 = 0; i2 =1; i3=0;
