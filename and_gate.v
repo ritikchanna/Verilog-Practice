e4b54e75470546a14acccea325d6426a9c6dfe23
Ritik Channa <chnritik@gmail.com>
2018-08-14 09:46:42 +0530

    n-input and gate using gate level modelling

--- and_gate.v
+++ and_gate.v
@@ -1,9 +1,21 @@
 `timescale 1ns / 1ps
 //2-input and-gate using gate level modelling.
-module and_gate(o,a,b);
-input a,b;
+module and_gate(o,i);
+parameter inputs = 2;
+input [inputs:1] i;
 output o;
-and a1(o,a,b);
+generate case(inputs)
+2: and a2 (o,i[2],i[1]);
+3: and a3 (o,i[3],i[2],i[1]);
+4: and a4 (o,i[4],i[3],i[2],i[1]);
+5: and a5 (o,i[5],i[4],i[3],i[2],i[1]);
+6: and a6 (o,i[6],i[5],i[4],i[3],i[2],i[1]);
+7: and a7 (o,i[7],i[6],i[5],i[4],i[3],i[2],i[1]);
+8: and a8 (o,i[8],i[7],i[6],i[5],i[4],i[3],i[2],i[1]);
+9: and a9 (o,i[9],i[8],i[7],i[6],i[5],i[4],i[3],i[2],i[1]);
+10: and a10 (o,i[10],i[9],i[8],i[7],i[6],i[5],i[4],i[3],i[2],i[1]);
+endcase
+endgenerate
 endmodule
 
 //n-input and-gate using dataflow modelling.
